b955fb87e12f794bf06b2872fa5eaf67cd7663cd
erikyuzwa <erikyuzwa@gmail.com>
2024-03-25 18:26:56 -0600

    initial kaboom!

---
+++ src/test/java/com/example/springboot3javamailwithmailhog/SpringBoot3JavaMailWithMailhogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3javamailwithmailhog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3JavaMailWithMailhogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
